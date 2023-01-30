9ae962655ddae5706cdcdc498e2439bf0cf5f368
“Koluka” <koluka78@mail.ru>
2023-01-30 18:30:33 +0300

    add out gitignore

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         int account = 100;
-        int summa = 1100;
+        int summa = 1001;
         int bonus = 100;
 
 
-        if (summa >= 1000) {
+        if (summa > 1000) {
             System.out.println("Итоговый счет:" + (account + summa + summa / bonus));
             System.out.println("Сумма бонусных рублей:" + (summa / bonus));
 
