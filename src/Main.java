2323889bde3fb9c470328296abb27e33692bb013
“Koluka” <koluka78@mail.ru>
2023-01-30 12:17:59 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int account = 100;
+        int summa = 1100;
+        int bonus = 100;
+
+
+        if (summa >= 1000) {
+            System.out.println("Итоговый счет:" + (account + summa + summa / bonus));
+            System.out.println("Сумма бонусных рублей:" + (summa / bonus));
+
+        } else {
+            System.out.println("Итоговый счет:" + (account + summa));
+            System.out.println("Бонусы не начислены");
+        }
+
+
+    }
+}
